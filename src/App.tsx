1b18e75fb1b1dc9a3e5972bc9b56f9bc1afc7f03
aki <lala.akira@gmail.com>
2021-10-26 09:10:01 +0900

    Add windicss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import 'bootstrap/scss/bootstrap.scss'
+import 'virtual:windi.css'
 
 function App() {
   return (
     <div className="container py-3">
-      <div className="text-center">hoge</div>
+      <div className="text-center tw-text-3xl">hoge</div>
     </div>
   )
 }
