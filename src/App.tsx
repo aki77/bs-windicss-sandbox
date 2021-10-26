12f3d26bb9513a91892661d2f7e3ad51039dfd7f
aki <lala.akira@gmail.com>
2021-10-26 09:03:28 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'bootstrap/scss/bootstrap.scss'
+
+function App() {
+  return (
+    <div className="container py-3">
+      <div className="text-center">hoge</div>
+    </div>
+  )
+}
+
+export default App
